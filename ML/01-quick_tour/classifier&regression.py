efba67b6ea407824f161d676984e38cc9df01d13
lsmman <tmdgus507@naver.com>
2019-10-07 22:44:37 +0900

    classifier

--- ML/01-quick_tour/classifier&regression.py
+++ ML/01-quick_tour/classifier&regression.py
@@ -70,6 +70,8 @@ def plot_basic_data():
 '''
 B. 분류
 
+원점에서 CLASS_RADIUS 0.6를 분류 기준으로 두고
+SVM을 활용해 분류 분포를 학습한다.
 '''
 # 클래스의 임계값
 
@@ -123,57 +125,83 @@ def classifier():
 
 '''
 C. 회귀
-
+1차식, 2차식, 9차식을 회귀
 '''
+def regression():
+    from sklearn import linear_model
 
-from sklearn import linear_model
+    ### 1차식으로 회귀
 
-### 1차식으로 회귀
+    X1_TRAIN = x_train
+    X1_TEST = x_test
 
-X1_TRAIN = x_train
-X1_TEST = x_test
+    model = linear_model.LinearRegression()
+    model.fit(X1_TRAIN, y_train)
 
-model = linear_model.LinearRegression()
-model.fit(X1_TRAIN, y_train)
+    plt.plot(x_test, model.predict(X1_TEST), linestyle='-.', label='poly deg 1')
 
-plt.plot(x_test, model.predict(X1_TEST), linestyle='-.', label='poly deg 1')
+    ### 2차식으로 회귀
 
-### 2차식으로 회귀
+    X2_TRAIN = np.c_[x_train**2, x_train]
+    X2_TEST = np.c_[x_test**2, x_test]
 
-X2_TRAIN = np.c_[x_train**2, x_train]
-X2_TEST = np.c_[x_test**2, x_test]
+    model = linear_model.LinearRegression()
+    model.fit(X2_TRAIN, y_train)
 
-model = linear_model.LinearRegression()
-model.fit(X2_TRAIN, y_train)
+    plt.plot(x_test, model.predict(X2_TEST), linestyle='--', label='poly deg 2')
 
-plt.plot(x_test, model.predict(X2_TEST), linestyle='--', label='poly deg 2')
+    ### 9차식으로 회귀
 
-### 9차식으로 회귀
+    X9_TRAIN_list = tuple(x_train**pow_num for pow_num in range(1, 10))
+    X9_TRAIN = np.c_[X9_TRAIN_list]
+    X9_TEST_list = tuple(x_test**pow_num for pow_num in range(1, 10))
+    X9_TEST = np.c_[X9_TEST_list]
 
-X9_TRAIN_list = tuple(x_train**pow_num for pow_num in range(1, 10))
-X9_TRAIN = np.c_[X9_TRAIN_list]
-X9_TEST_list = tuple(x_test**pow_num for pow_num in range(1, 10))
-X9_TEST = np.c_[X9_TEST_list]
+    model = linear_model.LinearRegression()
+    model.fit(X9_TRAIN, y_train)
 
-model = linear_model.LinearRegression()
-model.fit(X9_TRAIN, y_train)
-
-plt.plot(x_test, model.predict(X9_TEST), linestyle='-',label='poly deg 9')
+    plt.plot(x_test, model.predict(X9_TEST), linestyle='-',label='poly deg 9')
 
 
-plt.scatter(x_train, y_train, c='black', s=30, marker='v',label='train')
-plt.scatter(x_test, y_test, c='black', s=30, marker='x',label='test')
+    plt.scatter(x_train, y_train, c='black', s=30, marker='v',label='train')
+    plt.scatter(x_test, y_test, c='black', s=30, marker='x',label='test')
 
-# 원래 선을 표시
-plt.plot(data_x, data_ty, linestyle=':', label='non noise curve')
+    # 원래 선을 표시
+    plt.plot(data_x, data_ty, linestyle=':', label='non noise curve')
 
-# x축/y축의 범위를 설정
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
+    # x축/y축의 범위를 설정
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
 
-# 범례의 표시 위치를 지정
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left',borderaxespad=0)
+    # 범례의 표시 위치를 지정
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left',borderaxespad=0)
 
-# 그래프를 표시
-plt.show()
+    # 그래프를 표시
+    plt.show()
 
+### 클러스터링
+def clustering():
+    from sklearn import cluster
+
+    data = np.c_[data_x, data_vy]
+
+    model = cluster.KMeans(n_clusters=3)
+    model.fit(data)
+
+    labels = model.labels_
+
+    plt.scatter(data_x[labels == 0], data_vy[labels == 0], marker='v', label='cluster 0')
+    plt.scatter(data_x[labels == 1], data_vy[labels == 1], marker='x', label='cluster 1')
+    plt.scatter(data_x[labels == 2], data_vy[labels == 2], marker='o', label='cluster 2')
+
+    plt.plot(data_x, data_ty, linestyle=':', label='non noise curve')
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+
+    # 범례의 표시 위치를 지정
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left',borderaxespad=0)
+
+    # 그래프를 표시
+    plt.show()
